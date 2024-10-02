0da092563e1a03751144775e54342957fab23380
troy600 <bazartroy531@gmail.com>
2024-10-02 12:26:49 +0800

    swayy

---
+++ install.sh
@@ -0,0 +1,27 @@
+mkdir -p ~/.config/hypr ~/.config/alacritty ~/.config/waybar ~/.config/htop
+cp ./hypr/* ~/.config/hypr/
+cp ./waybar/* ~/.config/waybar/
+cp ./alacritty/* ~/.config/alacritty/
+
+
+
+sudo pacman -Syu --needed pulseaudio sway waybar sddm base-devel git alacritty python-pywal
+
+if [ -f /usr/bin/yay ]; then
+    echo "installing..."
+else
+    git clone https://aur.archlinux.org/yay
+    cd yay/
+    makepkg -si
+fi
+
+echo "removing yay folder"
+rm -rfv yay
+
+yay -S pacseek --needed
+
+mkdir ~/wallpaper -p && mv ./wallpapers/* ~/wallpaper/
+cd ~/wallpaper/ && tar -xvf aa.tar
+wal -i ~/wallpaper/Aiabstract.png --saturate 1
+ln -s ~/.cache/wal/colors-waybar.css ~/.config/waybar/colors.css
+echo "try logging in and out"
